1ce8593b1fda3a3a41037cb310f965ee33b00965
sajjadhossain <sajjadhossain0756@gmail.com>
2025-01-20 23:37:30 +0600

    article status declined when click decline btn done

--- src/pages/dashbord/AllArticle.jsx
+++ src/pages/dashbord/AllArticle.jsx
@@ -5,11 +5,11 @@ import Swal from 'sweetalert2';
 import useAxiosSecure from '../../hooks/useAxiosSecure';
 
 const AllArticle = () => {
-  const [articleData,refetch] = useArticleData()
+  const [articleData, refetch] = useArticleData()
   const axiosSecure = useAxiosSecure()
 
   // approve article
-  const handleApproveArticle = (article) =>{
+  const handleApproveArticle = (article) => {
     axiosSecure.patch(`/all_articles/approve/${article._id}`)
       .then(res => {
         if (res.data.modifiedCount > 0) {
@@ -18,6 +18,22 @@ const AllArticle = () => {
             position: "top-end",
             icon: "success",
             title: `${article?.title} is Approved`,
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
+      })
+  }
+  // decline article
+  const handleDeclineArticle = (article) => {
+    axiosSecure.patch(`/all_articles/decline/${article._id}`)
+      .then(res => {
+        if (res.data.modifiedCount > 0) {
+          refetch()
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${article?.title} is Declined`,
             showConfirmButton: false,
             timer: 1500
           });
@@ -50,15 +66,15 @@ const AllArticle = () => {
                 <td><img src={article?.authorPhoto} alt="profile" className='h-12 w-12 rounded-full' /></td>
                 <td>{article?.authorName}</td>
                 <td>{article?.authorEmail}</td>
-                <td>{article?.title.substring(1,30)}....</td>
+                <td>{article?.title.substring(1, 30)}....</td>
                 <td>{article?.category}</td>
-                <td>{format(new Date(article?.postedDate),"PPP p")}</td>
+                <td>{format(new Date(article?.postedDate), "PPP p")}</td>
                 <td>{article?.status}</td>
                 <td>
-                    <button onClick={()=>{handleApproveArticle(article)}} className='border p-1 bg-green-400 w-24'>Approve</button>
-                    <button className='border p-1 bg-red-400 w-24'>Decline</button>
-                    <button className='border p-1 bg-green-400 w-24'>Premium</button>
-                    <button className='border p-1 bg-red-400 w-24'>Delete</button>
+                  <button onClick={() => { handleApproveArticle(article) }} className='border p-1 bg-green-400 w-24'>Approve</button>
+                  <button onClick={() => { handleDeclineArticle(article) }} className='border p-1 bg-red-400 w-24'>Decline</button>
+                  <button className='border p-1 bg-green-400 w-24'>Premium</button>
+                  <button className='border p-1 bg-red-400 w-24'>Delete</button>
                 </td>
                 {/* <td>
                   {user.role === 'admin' ? 'Admin' : <button onClick={() => handleMakeAdmin(user)} className='btn bg-orange-400 text-white' >
